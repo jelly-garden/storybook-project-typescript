413670c22a5a65c777bd2a312fc91d879d5a1238
장정원 <longthegarden@innodep.com>
2022-09-06 17:59:23 +0900

    복합적 컴포넌트 조합하기  - 상태(States) 구현하기

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,7 +14,7 @@ interface TaskListProps {
 }
 
 export default function TaskList({
-  loading,
+  loading = false,
   tasks,
   onPinTask,
   onArchiveTask,
@@ -23,25 +23,45 @@ export default function TaskList({
     onPinTask,
     onArchiveTask,
   };
-
+  const LoadingRow = (
+    <div className="loading-item">
+      <span className="glow-checkbox" />
+      <span className="glow-text">
+        <span>Loading</span> <span>cool</span> <span>state</span>
+      </span>
+    </div>
+  );
   if (loading) {
     return (
       <div className="list-items" data-testid="loading" key={"loading"}>
-        loading
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
       </div>
     );
   }
   if (tasks.length === 0) {
     return (
       <div className="list-items" key={"empty"} data-testid="empty">
-        empty
+        <div className="wrapper-message">
+          <span className="icon-check" />
+          <div className="title-message">You have no tasks</div>
+          <div className="subtitle-message">Sit back and relax</div>
+        </div>
       </div>
     );
   }
 
+  const tasksInOrder = [
+    ...tasks.filter((t) => t.state === "TASK_PINNED"),
+    ...tasks.filter((t) => t.state !== "TASK_PINNED"),
+  ];
   return (
     <div className="list-items">
-      {tasks.map((task) => (
+      {tasksInOrder.map((task) => (
         <Task key={task.id} task={task} {...events} />
       ))}
     </div>
