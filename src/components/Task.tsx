e51447b07bc75f4fa723d3751a7f766706834d09
장정원 <longthegarden@innodep.com>
2022-09-06 17:09:14 +0900

    불필요 소스 제거  - 데이터 요구 사항 명시하기

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import PropTypes from "prop-types";
 
 interface TaskProps {
   /** Composition of the task */
@@ -63,19 +62,3 @@ export default function Task({
     </div>
   );
 }
-
-Task.propTypes = {
-  /** Composition of the task */
-  task: PropTypes.shape({
-    /** Id of the task */
-    id: PropTypes.string.isRequired,
-    /** Title of the task */
-    title: PropTypes.string.isRequired,
-    /** Current state of the task */
-    state: PropTypes.string.isRequired,
-  }),
-  /** Event to change the task to archived */
-  onArchiveTask: PropTypes.func,
-  /** Event to change the task to pinned */
-  onPinTask: PropTypes.func,
-};
